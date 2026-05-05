4d9aeb2c10dd91e8ed0acedc001c38dc9564d7fc
Yoshyyy2 <igitkarl0@gmail.com>
2026-05-05 08:49:30 +0800

    Update API_URL to new endpoint

--- stripebot.py
+++ stripebot.py
@@ -18,7 +18,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 BOT_TOKEN  = '8603869007:AAGO09lmBqufzrE2TrkATvr8FMrRVKCirPg'
 ADMIN_ID   = 6601184733          # your Telegram user ID
 API_KEY    = 'afuona_2026'
-API_URL    = 'https://stripeeapi.onrender.com'  # change to your hosted API URL
+API_URL    = 'https://stripechkapi.onrender.com'  # change to your hosted API URL
 # ──────────────────────────────────────────────────────────
 
 bot = telebot.TeleBot(BOT_TOKEN, num_threads=10)
